91abf26955e0d7788209099cec47b2ae30f002f1
zerohouse <parksungho86@gmail.com>
2015-08-09 20:26:23 +0900

    go

--- app/js/creative.js
+++ app/js/creative.js
@@ -62,5 +62,17 @@
     }
 
 
-
 })(jQuery); // End of use strict
+
+
+$(document).ready(function () {
+    $('#sticky-navigation').scrollspy({
+        min: $('#nav').offset().top,
+        onEnter: function (element, position) {
+            $("#nav").addClass('fixed');
+        },
+        onLeave: function (element, position) {
+            $("#nav").removeClass('fixed');
+        }
+    });
+});
